58520b97ce77b94b6ebf91a9ce2ffced37dc3a39
rakeshsajjarao07 <sajjaraorakesh123@gmail.com>
2025-06-13 21:46:16 +0530

    initial commit

---
+++ pizza2/src/main/java/com/example/pizza/entity/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.example.pizza.entity;
+
+public enum OrderStatus {
+    PENDING,    // Order is placed but not yet processed
+    SHIPPED,    // Order has been shipped
+    DELIVERED   // Order has been delivered to the customer
+}
