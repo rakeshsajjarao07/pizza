a2fcc1bc56c81fed963e34a28818c3a694a339c5
Sajjarao Rakesh <sajjaraorakesh123@gmail.com>
2025-06-13 22:03:38 +0530

    Delete pizza directory

--- pizza/src/main/java/com/example/pizza/entity/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.pizza.entity;
-
-public enum OrderStatus {
-    PENDING,    // Order is placed but not yet processed
-    SHIPPED,    // Order has been shipped
-    DELIVERED   // Order has been delivered to the customer
-}
--- pizza/src/main/java/com/example/pizza/enums/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.example.pizza.enums;
